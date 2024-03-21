3b2bd2aeb8bc2aaad6b8087ac0cfedbad3dfd1b1
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-03-21 19:18:56 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 
-import { App } from "./App";
+import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
